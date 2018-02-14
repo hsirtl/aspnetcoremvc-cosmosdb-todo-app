68d8df1b6f357c550e5e261cbe669991093fb0f2
Holger Sirtl <hsirtl@microsoft.com>
2018-02-14 15:22:04 +0100

    Initial commit

---
+++ AspNetCoreMvcTodoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreMvcTodoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
